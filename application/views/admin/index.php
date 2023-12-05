ebf368becebfcfb5956f67aa88e26e9f42bb2c81
sendiramadhan <sendiramadhan084@gamil.com>
2023-12-05 16:16:12 +0700

    upload

---
+++ application/views/admin/index.php
@@ -0,0 +1,113 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+  <!-- row ux-->
+  <div class="row">
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-left-danger shadow h-100 py-2 bgprimary">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="text-md font-weight-bold text-white textuppercase mb-1">Jumlah Anggota</div>
+              <div class="h1 mb-0 font-weight-bold text-white"><?=
+                                                                $this->ModelUser->getUserWhere(['role_id' => 1])->num_rows();
+                                                                ?></div>
+            </div>
+            <div class="col-auto">
+              <a href="<?= base_url('user/anggota'); ?>"><i class="fas fa-users fa-3x text-warning"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-left-primary shadow h-100 py-2 bgwarning">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="text-md font-weight-bold text-white textuppercase mb-1">Stok Buku Terdaftar</div>
+              <div class="h1 mb-0 font-weight-bold text-white">
+                <?php
+                $where = ['stok != 0'];
+                $totalstok = $this->ModelBuku->total(
+                  'stok',
+                  $where
+                );
+                echo $totalstok;
+                ?>
+              </div>
+            </div>
+            <div class="col-auto">
+              <a href="<?= base_url('buku'); ?>"><i class="fas fabook fa-3x text-primary"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-left-success shadow h-100 py-2 bgdanger">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="text-md font-weight-bold text-white textuppercase mb-1">Buku yang dipinjam</div>
+              <div class="h1 mb-0 font-weight-bold text-white">
+                <?php
+                $where = ['dipinjam != 0'];
+                $totaldipinjam = $this->ModelBuku->total(
+                  'dipinjam',
+                  $where
+                );
+                echo $totaldipinjam;
+                ?>
+              </div>
+            </div>
+            <div class="col-auto">
+              <a href="<?= base_url('user'); ?>"><i class="fas fauser-tag fa-3x text-success"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card border-left-warning shadow h-100 py-2 bgsuccess">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="text-md font-weight-bold text-white textuppercase mb-1">Buku yang dibooking</div>
+              <div class="h1 mb-0 font-weight-bold text-white">
+                <?php
+                $where = ['dibooking !=0'];
+                $totaldibooking = $this->ModelBuku->total('dibooking', $where);
+                echo $totaldibooking;
+                ?>
+              </div>
+            </div>
+            <div class="col-auto">
+              <a href="<?= base_url('user'); ?>"><i class="fas fashopping-cart fa-3x text-danger"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- end row ux-->
+  <!-- Divider -->
+  <hr class="sidebar-divider">
+  <!-- row table-->
+  <div class="row">
+    <div class="table-responsive table-bordered col-sm-5 ml-auto mrauto mt-2">
+      <div class="page-header">
+        <span class="fas fa-users text-primary mt-2 "> Data
+          User</span>
+        <a class="text-danger" href="<?php echo
+                                      base_url('user/data_user'); ?>"><i class="fas fa-search mt-2 floatright"> Tampilkan</i></a>
+      </div>
+      <table class="table mt-3">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nama Anggota</th>
+            <th>Email</th>
+            <th>Role ID</th>
+            <th>Aktif</th>
+            <th>Member Sejak</th>
+          </tr>
